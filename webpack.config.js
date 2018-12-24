34c38becac26f7bf8f2d98a5c1486e65bcce01ee
Werner1201 <werner.romling@unigranrio.br>
2018-12-24 16:09:05 -0200

    Modificacoes

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,23 @@
 module.exports = {
-    entry: ['@babel/polyfill','./src/main.js'],
-    output: {
-        path: __dirname + '/public', 
-        filename: 'bundle.js',
-    },
-    devServer: {
-        contentBase: __dirname + '/public',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                }
-            },
-        ],
-    }, 
-};
+  entry: ['@babel/polyfill', './src/main.js'],
+  output: {
+    // eslint-disable-next-line no-path-concat
+    path: __dirname + '/public',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    // eslint-disable-next-line no-path-concat
+    contentBase: __dirname + '/public'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }
+}
